da9c2d0d9ae4e7d6146e7699c4a71e8df3423404
ngo610 <ngo610@uw.edu>
2023-02-18 15:56:26 -0800

    create sub dataframe for table

--- table.R
+++ table.R
@@ -1,1 +1,22 @@
-# Table file
+# Natalie Olson
+# Table
+
+# Load packages
+library(tidyverse)
+library(dplyr)
+library(readr)
+
+# Load data set
+IMDB_Horror_movies <- read_csv("~/info201/archive/IMDB Horror movies.csv")
+
+# Create dataframe based on key columns
+# Grouped by top movie rating in the US
+horror_movie_df <- IMDB_Horror_movies %>% 
+  select(Title, `Review Rating`, `Release Date`, Budget, `Release Country`) %>% 
+  group_by(`Review Rating`) %>% 
+  filter(!is.na(`Review Rating`)) %>% 
+  filter(!is.na(Budget)) %>% 
+  filter(`Release Country` == "USA") %>% 
+  relocate(`Review Rating`) %>% 
+  arrange(desc(`Review Rating`)) %>% 
+  head(n=10)
